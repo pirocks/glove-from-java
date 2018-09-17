b57db73f929138d874e2c97c0cf37d661a0daf61
Francis Nixon <13nixonf@gmail.com>
2018-09-16 23:56:24 -0700

    Initial creation of gitignore and directory structure w/ unimplmented class

--- src/main/kotlin/uk/ac/ic/doc/fpn17/GloveCOriginal.kt
+++ src/main/kotlin/uk/ac/ic/doc/fpn17/GloveCOriginal.kt
@@ -1,2 +1,12 @@
 package uk.ac.ic.doc.fpn17
 
+import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer
+import org.deeplearning4j.models.word2vec.Word2Vec
+import java.io.File
+
+class GloveCOriginal{
+    fun runBlocking(): Word2Vec {
+
+        return WordVectorSerializer.readWord2VecModel(File(workingDirectory.absolutePath, "vector_output.txt"))
+    }
+}
