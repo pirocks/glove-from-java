fef8fa958217a56dc56c8113b6bd606fe2739161
Francis Nixon <13nixonf@gmail.com>
2018-09-20 23:00:30 -0700

    first working version. Haven't deployed to local repo yet.

--- src/main/kotlin/uk/ac/ic/doc/fpn17/GloveCOriginal.kt
+++ src/main/kotlin/uk/ac/ic/doc/fpn17/GloveCOriginal.kt
@@ -3,10 +3,82 @@ package uk.ac.ic.doc.fpn17
 import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer
 import org.deeplearning4j.models.word2vec.Word2Vec
 import java.io.File
+import java.nio.charset.Charset
 
-class GloveCOriginal{
-    fun runBlocking(): Word2Vec {
-
-        return WordVectorSerializer.readWord2VecModel(File(workingDirectory.absolutePath, "vector_output.txt"))
+class GloveCOriginal(
+        val data : Collection<Collection<String>>,
+        val memoryGB : Double = 4.0,
+        val maxIter: Int = 50,
+        val embeddingSize: Int = 50,
+        val alpha: Double = 0.75,
+        val windowSize: Int = 15,
+        val vocabMinCount: Int = 5,
+        val workingDirectory: String = "/mnt/harddrive1/cglove-test/"//System.getProperty("java.io.tmpdir") + "/working"
+){
+    init {
+        File(workingDirectory).mkdirs()
+        extractToWorkingDirectory()
+        val vocab = mutableMapOf<String,Int>()
+        val dataFileContents = data.parallelStream().map {
+            it.reduce {
+                acc, s -> "$acc $s"
+            }
+        }.map { it + "\n" }.reduce { acc, s -> "$acc$s"}
+        File(workingDirectory,"data.txt").writeText(dataFileContents.orElseThrow { IllegalArgumentException("Something was wrong with the data provided. Make sure there are no empty collections inputted.") })
     }
+
+    fun runBlocking(): Word2Vec {
+        val pb = ProcessBuilder(File(workingDirectory,"run.sh").path);
+        pb.directory(File(workingDirectory))
+        pb.redirectError(ProcessBuilder.Redirect.INHERIT)
+        pb.redirectOutput(ProcessBuilder.Redirect.INHERIT)
+        val p = pb.start();
+
+        return WordVectorSerializer.readWord2VecModel(File(workingDirectory, "vector_output.txt"))
+    }
+
+    fun extractToWorkingDirectory(){
+        extractResource("run.sh", mapOf(
+                Pair("ALPHA=0.75","ALPHA=$alpha"),
+                Pair("MEMORY=4.0","MEMORY=$memoryGB"),
+                Pair("EMBEDDING_SIZE=50","EMBEDDING_SIZE=$embeddingSize"),
+                Pair("MAX_ITER=15","MAX_ITER=$maxIter"),
+                Pair("WINDOW_SIZE=15","WINDOW_SIZE=$windowSize"),
+                Pair("VOCAB_MIN_COUNT=5","VOCAB_MIN_COUNT=$vocabMinCount")
+        ))
+        extractResource("GloVe/build/cooccur")
+        extractResource("GloVe/build/glove")
+        extractResource("GloVe/build/shuffle")
+        extractResource("GloVe/build/vocab_count")
+    }
+
+    fun extractResource(resource: String, replaceRules: Map<String, String> = emptyMap()){
+        val `package` = "uk/ac/ic/doc/fpn17/"
+        val resourceBytes = javaClass.classLoader.getResourceAsStream(`package` + resource).readBytes()
+        val file = File(workingDirectory, resource)
+        file.parentFile.mkdirs()
+        if(replaceRules.isEmpty()){
+            file.writeBytes(resourceBytes)
+            return
+        }
+        var resourceContents = resourceBytes.toString(Charset.defaultCharset())
+        replaceRules.entries.forEach { resourceContents = resourceContents.replace(it.key,it.value) }
+        file.writeText(resourceContents)
+    }
+}
+
+fun main(args: Array<String>) {
+    GloveCOriginal(arrayListOf(
+            arrayListOf("a", "b", "c", "a", "a", "b", "c", "a", "a", "b", "c", "a", "a", "b", "c", "a")
+            , arrayListOf("a", "b", "a", "c", "a", "b", "a", "c", "a", "b", "a", "c", "a", "b", "a", "c")
+            , arrayListOf("a", "c", "b", "a", "a", "c", "b", "a", "a", "c", "b", "a", "a", "c", "b", "a")
+            , arrayListOf("a", "b", "c", "a", "a", "b", "c", "a", "a", "b", "c", "a", "a", "b", "c", "a")
+            , arrayListOf("a", "b", "a", "c", "a", "b", "a", "c", "a", "b", "a", "c", "a", "b", "a", "c")
+            , arrayListOf("a", "c", "b", "a", "a", "c", "b", "a", "a", "c", "b", "a", "a", "c", "b", "a")
+            , arrayListOf("a", "b", "a", "c", "a", "b", "a", "c", "a", "b", "a", "c", "a", "b", "a", "c")
+            , arrayListOf("a", "a", "c", "b", "a", "a", "c", "b", "a", "a", "c", "b", "a", "a", "c", "b")
+            , arrayListOf("a", "c", "a", "b", "a", "c", "a", "b", "a", "c", "a", "b", "a", "c", "a", "b")
+            , arrayListOf("a", "b", "a", "c", "a", "b", "a", "c", "a", "b", "a", "c", "a", "b", "a", "c")
+            , arrayListOf("a", "a", "b", "c", "a", "a", "b", "c", "a", "a", "b", "c", "a", "a", "b", "c")
+            , arrayListOf("a", "b", "c", "a", "a", "b", "c", "a", "a", "b", "c", "a", "a", "b", "c", "a"))).runBlocking()
 }
